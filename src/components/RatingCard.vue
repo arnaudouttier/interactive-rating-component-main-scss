78fd49eb046cad7f086264d7f2b05083a8aabfe8
arnaudouttier <arnoutti@gmail.com>
2022-03-25 17:55:48 +0100

    prm beta

--- src/components/RatingCard.vue
+++ src/components/RatingCard.vue
@@ -1,35 +1,39 @@
 <template>
-  <div class="raiting-card" v-if="!show">
-    <div class="card-icon">
-      <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z"
-          fill="#FC7614"
-        />
-      </svg>
+  <div class="card">
+    <div class="raiting-card" v-if="!show">
+      <div class="card-icon">
+        <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z"
+            fill="#FC7614"
+          />
+        </svg>
+      </div>
+      <h2 class="card-title">How did we do?</h2>
+      <p class="card-content">
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <div class="card-raiting">
+        <button
+          v-for="n in 5"
+          :key="n"
+          class="raiting-btn"
+          :value="n"
+          @click="getCurrentRaiting(n)"
+        >
+          {{ n }}
+        </button>
+      </div>
+      <button @click="show = !show" class="card-submit">Submit</button>
+      {{ crRaiting }}
     </div>
-    <h2 class="card-title">How did we do?</h2>
-    <p class="card-content">
-      Please let us know how we did with your support request. All feedback is
-      appreciated to help us improve our offering!
-    </p>
-    <div class="card-raiting">
-      <button
-        v-for="n in 5"
-        :key="n"
-        class="raiting-btn"
-        :value="n"
-        @click="getCurrentRaiting(n)"
-      >
-        {{ n }}
-      </button>
-    </div>
-    <button @click="show = !show" class="card-submit">Submit</button>
-    {{ crRaiting }}
+    <Transition name="slide-fade">
+      <div class="result-card" v-if="show">
+        <ResultCard :currentRaiting="crRaiting" />
+      </div>
+    </Transition>
   </div>
-  <Transition name="slide-fade">
-    <ResultCard :currentRaiting="crRaiting" v-if="show" />
-  </Transition>
 </template>
 
 <script>
@@ -54,22 +58,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.raiting-card{
+.card {
   border: 2px solid red;
-  color : $Medium_Grey;
-  margin: 0 $card_margin ;
+  color: $Medium_Grey;
+  margin: 0 $card_margin;
   padding: $card_padding;
 }
 
-.card-icon, .card-title, .card-raiting{
+.card-icon,
+.card-title,
+.card-raiting {
   margin-bottom: $margin_md;
 }
 
-.card-content{
+.card-content {
   margin-bottom: $margin_xxl;
 }
 
-.card-submit{
+.card-submit {
   margin-bottom: $margin_sm;
 }
 
